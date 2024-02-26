845c9230560fee6236853fb9fb87530504cd0bbc
unknown <marceloluigi12.p8@gmail.com>
2024-02-26 13:02:45 -0300

    estutando sobreescrita

---
+++ src/javaCore/Sobreescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package javaCore.Sobreescrita.test;
+
+import javaCore.Sobreescrita.domain.Anime;
+
+public class AnimeTest01 {
+	public static void main(String[] args) {
+		Anime anime = new Anime("Dr. Stone");
+		
+		System.out.println(anime);
+	}
+}
