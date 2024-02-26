ef20b1e50fe00623a3124a30f856eea75adf5926
unknown <marceloluigi12.p8@gmail.com>
2024-02-26 18:35:49 -0300

    Trabalhando com Enumerações e toString para retorno de dados seguros de sujeiras

--- src/javaCore/Kenum/domain/Cliente.java
+++ src/javaCore/Kenum/domain/Cliente.java
@@ -1,39 +1,27 @@
 package javaCore.Kenum.domain;
 
 public class Cliente {
+	public enum TipoPagamento{
+		DEBITO, CREDITO
+	}
 	private String nome;
 	private TipoCliente tipoCliente;
+	private TipoPagamento tipoPagamento;
 	
-	
-	public Cliente(String nome, TipoCliente tipoCliente) {
+	public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
 		this.nome = nome;
 		this.tipoCliente = tipoCliente;
+		this.tipoPagamento = tipoPagamento;
 	}
-	
 
 	@Override
 	public String toString() {
-		return "Cliente [nome=" + nome + ", tipoCliente=" + tipoCliente + "]";
+		return "Cliente [nome=" + nome + 
+				", tipoCliente=" + tipoCliente.getRelatorio() + 
+				", tipoClienteInt=" + tipoCliente.VALOR + 
+					", tipoPagamento=" + tipoPagamento + 
+				"]";
 	}
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-	public TipoCliente getTipoCliente() {
-		return tipoCliente;
-	}
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-	public void setTipoCliente(TipoCliente tipoCliente) {
-		this.tipoCliente = tipoCliente;
-	}
+	
 	
 }
--- src/javaCore/Kenum/domain/TipoCliente.java
+++ src/javaCore/Kenum/domain/TipoCliente.java
@@ -1,6 +1,19 @@
 package javaCore.Kenum.domain;
 
 public enum TipoCliente {
-	PESSOA_FISICA,
-	PESSOA_JURIDICA
+	PESSOA_FISICA(1, "Pessoa Fisica"),
+	PESSOA_JURIDICA(2, "Pessoa Jurídica");
+	
+	public final int VALOR;
+	private String relatorio;
+	
+	TipoCliente(int valor, String relatorio){
+		this.VALOR = valor;
+		this.relatorio = relatorio;
+	}
+
+	public String getRelatorio() {
+		return relatorio;
+	}	
+	
 }
