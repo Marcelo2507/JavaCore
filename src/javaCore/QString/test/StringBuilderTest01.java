5fcf94b2bc9540b73fc95694d3f2f9a15280f3b7
unknown <marceloluigi12.p8@gmail.com>
2024-03-16 18:13:26 -0300

    Classes utilitarias — DATE, CALENDAR

--- src/javaCore/QString/test/StringBuilderTest01.java
+++ src/javaCore/QString/test/StringBuilderTest01.java
@@ -2,10 +2,12 @@ package javaCore.QString.test;
 
 public class StringBuilderTest01 {
 	public static void main(String[] args) {
+//		STRINGS SÃO IMUTAVEIS
 		String nome = "Willian Suane";
 		nome.concat(" Devdojo");
 		System.out.println(nome);
 		
+//		STRINGS DUILDER/BUFFER SÃO MUTAVEIS
 		StringBuilder sb = new StringBuilder("Willian Suane");
 		sb.append(" DevDojo").append(" Academy");
 		sb.reverse();
---
+++ src/javaCore/RDdates/test/DateTest01.java
@@ -0,0 +1,11 @@
+package javaCore.RDdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+	public static void main(String[] args) {
+		Date date = new Date();// long 100000
+		
+		System.out.println(date);
+	}
+}
